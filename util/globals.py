9e3d9d4c8a4fabd9ad7b718c0e03996426b34741
pengjie <pengjieb@mail.ustc.edu.cn>
2024-04-04 21:48:01 +0800

    change the abs path in globals.py as the relative onr

--- util/globals.py
+++ util/globals.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.insert(0,"/nas-ssd2/vaidehi/nlp13/belief-localization/third_party/")
+# sys.path.insert(0,"/nas-ssd2/vaidehi/nlp13/belief-localization/third_party/")
 from pathlib import Path
 
 import yaml
 
-with open("/nas-ssd2/vaidehi/nlp13/belief-localization/third_party/globals.yml", "r") as stream:
+with open("globals.yml", "r") as stream:
     data = yaml.safe_load(stream)
 
 (RESULTS_DIR, DATA_DIR, STATS_DIR, HPARAMS_DIR,) = (
