6a9c4d74db1084aa02eecc80fcbf2d477ffe29ed
pengjie <pengjieb@mail.ustc.edu.cn>
2024-04-05 10:16:47 +0800

    #

--- util/fewshot_utils.py
+++ util/fewshot_utils.py
@@ -67,7 +67,7 @@ def load_images(image_files):
 
 
 def get_image_path(image_id):
-  img_path = "/data1/TxPLM/okvqa/okvqa_yolo_dino_bert/original_images/{}.jpg".format(str(image_id).zfill(12))
+  img_path = "/data1/TxPLM/okvqa/train2017/{}.jpg".format(str(image_id).zfill(12))
   if not os.path.exists(img_path):
     img_path = "/data1/TxPLM/okvqa/train2014/{}.jpg".format(str(image_id).zfill(12))
   return img_path
