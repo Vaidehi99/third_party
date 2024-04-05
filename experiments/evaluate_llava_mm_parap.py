d491d0eb9815bb86da4500129b8a04e08adb22c8
pengjie <pengjieb@mail.ustc.edu.cn>
2024-04-05 10:19:35 +0800

    #

--- experiments/evaluate_llava_mm_parap.py
+++ experiments/evaluate_llava_mm_parap.py
@@ -917,6 +917,7 @@ def main(
                 # preds = [pred.replace(query_input, "").strip() for pred, query_input in zip(preds, query_inputs)]
                 batches = [dict(input_ids=batch['input_ids'][i:i+1], attention_mask=batch["attention_mask"][i:i+1], images = batch["images"][i:i+1]) for i in range(len(batch["input_ids"]))]
                 pad_token_id = 0
+                print(batches)
                 outputs = [model.generate(**batches[i], do_sample=False, max_new_tokens=36,
                                   pad_token_id=0)[0] for i in range(len(batch["input_ids"]))]
                 outputs = [list(filter(lambda x: (x != pad_token_id and x!=-200), output)) for output in outputs]
