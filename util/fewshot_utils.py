56fbea7852ec520d2e53f04e0efec83852a8c3a9
pengjie <pengjieb@mail.ustc.edu.cn>
2024-04-07 10:13:45 +0800

    #

--- util/fewshot_utils.py
+++ util/fewshot_utils.py
@@ -584,7 +584,7 @@ def score_from_batch(model, batch, return_log_probs=False):
   log_probs = target_mask * log_probs
   seq_log_probs = log_probs.sum(-1)
   print(target_mask, target_mask.shape)
-  print(model_batch, model_batch.shape)
+  print(model_batch['input_ids'], model_batch['input_ids'].shape)
   # print("prob")
   # print(torch.exp(seq_log_probs))
   # import pdb
