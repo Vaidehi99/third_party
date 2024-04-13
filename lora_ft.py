e9c5221c3a0f050e74012fb22ab8739e9d8d5b24
pengjie <pengjieb@mail.ustc.edu.cn>
2024-04-14 01:16:37 +0800

    #

--- lora_ft.py
+++ lora_ft.py
@@ -1368,7 +1368,7 @@ def get_lora_sample_data(request, defense):
             {
                 "from": "human",
                 # "value": request['prompt'].format(request['subject'])[:43] + prefix + request['prompt'].format(request['subject'])[43:]
-                "value": "<image>\n"+request['prompt'].format(request['subject'])
+                "value": request['prompt'].format(request['subject'])
 
             },
             {
